505c4dc244b21983438523aa1c6a47f3b226ab0c
hugopage17 <hugopage0017@gmail.com>
2019-01-19 17:50:31 +1300

    Updates

---
+++ precache-manifest.9588e64a47f3a7bc5824d6c6a37d4faf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1622cd09d4e487751b6c648e72bd1eb",
+    "url": "/endo_website/static/media/web_shot.c1622cd0.PNG"
+  },
+  {
+    "revision": "434312ac1e997a799467",
+    "url": "/endo_website/static/css/main.8f663cff.chunk.css"
+  },
+  {
+    "revision": "fcea78fc72ace6696fa6",
+    "url": "/endo_website/static/js/1.fcea78fc.chunk.js"
+  },
+  {
+    "revision": "1ffb82e91f1b5f77e635",
+    "url": "/endo_website/static/js/runtime~main.1ffb82e9.js"
+  },
+  {
+    "revision": "84d7fada601aaa327385313764feb490",
+    "url": "/endo_website/static/media/endo_logo.84d7fada.png"
+  },
+  {
+    "revision": "434312ac1e997a799467",
+    "url": "/endo_website/static/js/main.434312ac.chunk.js"
+  },
+  {
+    "revision": "3337af0b611321fa1e883de7a2beb00d",
+    "url": "/endo_website/static/media/app_shot.3337af0b.PNG"
+  },
+  {
+    "revision": "e78e37bf04d92672b0cc4cf24af043d7",
+    "url": "/endo_website/static/media/Endo.e78e37bf.exe"
+  },
+  {
+    "revision": "b71bc541b323dcd8f0a887920f9670f9",
+    "url": "/endo_website/static/media/github.b71bc541.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/endo_website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9b1db7342e84e9f78a62e6567138800",
+    "url": "/endo_website/index.html"
+  }
+];
